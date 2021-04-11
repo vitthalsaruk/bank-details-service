bb4fdf50d338440ee843317b37d4d51071fff699
Vitthal Saruk <48468620+vitthalsaruk@users.noreply.github.com>
2021-04-11 07:49:08 +0000

    Add files via upload

---
+++ src/main/java/com/bankPayeeMgmt/bankdetailsservice/BankDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bankPayeeMgmt.bankdetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankDetailsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankDetailsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankPayeeMgmt/bankdetailsservice/BankDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankPayeeMgmt.bankdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
